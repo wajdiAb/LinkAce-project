8a4b77143da46f2f2fad0526f65a1172291eaca2
wajdiAb <wagde.abo164@gmail.com>
2025-08-09 13:46:07 +0300

    commiting the yaml's files

---
+++ scripts/start-linkace.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker compose down -v
+sleep 5
+docker compose up -d
+sleep 10
+docker exec -it linkace_dev-php-1 php artisan migrate --force
+docker exec -it linkace_dev-php-1 php artisan setup:complete
+docker exec -i linkace_dev-php-1 php artisan registeruser --admin <<EOF
+wajdi
+wagde.abo164@gmail.com
+password
+EOF
